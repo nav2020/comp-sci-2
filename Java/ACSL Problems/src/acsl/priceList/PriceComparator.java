df210a8552678762058d72f99bfa3cf24a8dafd7
DrewHiggins <drewhigginscom@gmail.com>
2014-05-14 13:14:07 -0400

    fixed indent

--- Java/ACSL Problems/src/acsl/priceList/PriceComparator.java
+++ Java/ACSL Problems/src/acsl/priceList/PriceComparator.java
@@ -8,22 +8,22 @@ public class PriceComparator implements Comparator {
 		Price p1 = (Price)price1;
 		Price p2 = (Price)price2;
 		if(p1.getStartDate().getMonth() < p2.getStartDate().getMonth()){
-            return -1;
-        }
-        else if(p1.getStartDate().getMonth() == p2.getStartDate().getMonth()){
-            if(p1.getStartDate().getDay() < p2.getStartDate().getDay()){
-                return -1;
-            }
-            else if(p1.getStartDate().getDay() == p2.getStartDate().getDay()){
-                return 0;
-            }
-            else{
-                return 1;
-            }
-        }
-        else{
-            return 1;
-        }
+	            return -1;
+	        }
+	        else if(p1.getStartDate().getMonth() == p2.getStartDate().getMonth()){
+	            if(p1.getStartDate().getDay() < p2.getStartDate().getDay()){
+	                return -1;
+	            }
+	            else if(p1.getStartDate().getDay() == p2.getStartDate().getDay()){
+	                return 0;
+	            }
+	            else{
+	                return 1;
+	            }
+	        }
+	        else{
+	            return 1;
+	        }
 	}
 
 }
